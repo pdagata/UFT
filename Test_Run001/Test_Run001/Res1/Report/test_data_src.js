0562101b609183a01461d3c8a5996f6b4615f07e
pdagata <96979882+pdagata@users.noreply.github.com>
2023-07-13 16:28:22 +0200

    .

---
+++ Test_Run001/Test_Run001/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\pierluca.dagata\\Documents\\VIRTUAL_SHARE\\GIT\\UFT\\Test_Run001\\Test_Run001\\Res1',namePairs:[]}]};
